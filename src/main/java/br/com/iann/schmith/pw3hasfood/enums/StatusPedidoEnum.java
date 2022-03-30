68bc94905467fb3aff8a1f2efc171c05b5e0af89
Iann <iannnschmith@gmail.com>
2022-03-30 14:09:33 -0300

    Criacao da enum StatusPedidoEnum

---
+++ src/main/java/br/com/iann/schmith/pw3hasfood/enums/StatusPedidoEnum.java
@@ -0,0 +1,10 @@
+package br.com.iann.schmith.pw3hasfood.enums;
+
+public enum StatusPedidoEnum {
+	realizado,
+	pago,
+	confirmado,
+	pronto,
+	saiu_para_entrega,
+	entregue;
+}
---
+++ src/main/java/br/com/iann/schmith/pw3hasfood/enums/package-info.java
@@ -0,0 +1,1 @@
+package br.com.iann.schmith.pw3hasfood.enums;
